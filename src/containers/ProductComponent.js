f5903bba3073e6ac616277864d206ce6ff9d0046
Chidambarkharvi <chidambarkharvi5@gmail.com>
2022-03-13 12:00:36 +0530

    redux

---
+++ src/containers/ProductComponent.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import {Link } from "react-router-dom"
+
+function ProductComponent() {
+  const products = useSelector((state) => state.allProducts.products);
+  const renderList = products.map((product) =>{
+  const {title , image , id , price,category} = product
+  return(
+    <div className="four column wide" key={id}>
+    <Link to={`product/${id}`}>
+    <div className="ui link cards">
+    <div className="card">
+      <div className="image"> 
+      <img src={image} alt={title} /></div>
+      <div className="content"> 
+        <div className="header">{title}</div>
+        <div className="meta prise">${price}</div>
+        <div className="meta "> {category} </div>
+        </div>
+      </div>
+    </div>
+    </Link>
+  </div>
+
+  )
+})
+  return (
+    <>
+      {renderList}
+    </>
+
+  );
+}
+
+export default ProductComponent;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+
+import {combineReducers} from "redux"
+import { ProductReducer,selectedProductReducer } from "./productReducer"
+
+ const reducers = combineReducers({
+    allProducts:ProductReducer,
+    product:selectedProductReducer
+})
+
+export default reducers
